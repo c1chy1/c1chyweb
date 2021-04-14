35f52a35e621291dbbe90d77a7ea63451fa2eb2b
Pablo <cichy08081987pp@gmail.com>
2021-04-14 21:34:38 +0200

    webp images compression

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,28 @@
 export default {
 
 
+
     generate: {
         fallback: '404.html'
     },
 
-    modules : ['@nuxtjs/tailwindcss',['fullpage-nuxt', { animate: true}], '@neneos/nuxt-animate.css',],
+    modules : [
+        ['@nuxtjs/tailwindcss'],
+        ['fullpage-nuxt', { animate: true}],
+        ['@neneos/nuxt-animate.css'],
+        ['@aceforth/nuxt-optimized-images']
+    ],
+
 
     plugins: [
-       { src: '~/plugins/fullpage', mode: 'client' }
+       { src: '~/plugins/fullpage', mode: 'client',  } ,
     ],
 
     loading: '~/components/loading.vue',
+
+    optimizedImages: {
+        optimizeImages: true
+    }
+
 
 }
