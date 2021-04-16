a83ac6bec78473abff241baa3c96cf3270e9dac6
Pablo <cichy08081987pp@gmail.com>
2021-04-16 19:50:29 +0200

    nuxt.config.js package.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,76 +1,39 @@
-
-
 export default {
 
 
-/*    build: {
-        extend(config, {isDev, isClient}) {
-            config.module.rules.forEach(rule => {
-                if (String(rule.test) === String(/\.(png|jpe?g|gif|svg|webp)$/)) {
-                    // add a second loader when loading images
-                    rule.use.push({
-                        loader: 'image-webpack-loader',
-                        options: {
-                            svgo: {
-                                plugins: [
-                                    // use these settings for internet explorer for proper scalable SVGs
-                                    // https://css-tricks.com/scale-svg/
-                                    { removeViewBox: false },
-                                    { removeDimensions: true }
-                                ]
-                            }
-                        }
-                    })
-                }
-            })
-        }
-    },*/
-
-
-
-    head:  {
-        title: 'c1chy.web',
-        meta: [
-            {charset: 'utf-8'},
-            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-            {
-                hid: 'Portfolio',
-                name: 'c1chy.de',
-                content: 'My Frontend experiences'
-            }
-        ],
-        link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}]
-    },
-
-
-    target: 'static',
 
 
     generate: {
-        fallback: '404.html',
+        fallback: '404.html'
     },
-    buildModules:[
-        ['@nuxtjs/tailwindcss'] ,
-        ['@aceforth/nuxt-optimized-images'],
-        ['nuxt-compress']
-    ],
-
-
 
     modules : [
+        ['@nuxtjs/tailwindcss'],
         ['fullpage-nuxt', { animate: true}],
         ['@neneos/nuxt-animate.css'],
+        ['@aceforth/nuxt-optimized-images'],
+        ['nuxt-compress',  {
+            gzip: {
+                threshold: 8192,
+            },
+            brotli: {
+                threshold: 8192,
+            },
+        },
 
+        ]
     ],
 
+
     plugins: [
-       { src: '~/plugins/fullpage', mode: 'client',  } ,
+        { src: '~/plugins/fullpage', mode: 'client',  } ,
     ],
 
     loading: '~/components/loading.vue',
 
     optimizedImages: {
         optimizeImages: true
-    },
+    }
+
 
 }
