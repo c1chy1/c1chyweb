5565fec4bc7d465f60871b0e16eda3d06e7d26ac
Pablo <cichy08081987pp@gmail.com>
2021-06-05 01:10:46 +0200

    manifest pwa #20

--- plugins/custom-service-worker.js
+++ plugins/custom-service-worker.js
@@ -6,7 +6,7 @@
 workbox.precaching.precacheAndRoute([
     { url: '/_nuxt/img/8dcefc7-540.webp', revision: 'abcd1234' },
     { url: '/_nuxt/img/d604ae9.webp', revision: 'abcd1234' },
-
+    { url: '/_nuxt/img/810eed3-400.webp', revision: 'abcd1234' },
 ]);
 
 
