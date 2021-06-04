0a941c54639726d4f8fb6459a843aaa6315e242e
Pablo <cichy08081987pp@gmail.com>
2021-06-05 00:48:17 +0200

    manifest pwa #14

--- plugins/custom-service-worker.js
+++ plugins/custom-service-worker.js
@@ -2,7 +2,7 @@
 
 
 
-/*
+
 workbox.precaching.precacheAndRoute([
     { url: '/_nuxt/img/8dcefc7-540.webp', revision: 'abcd1234' },
     { url: '/_nuxt/img/d604ae9.webp', revision: 'abcd1234' },
@@ -10,7 +10,7 @@ workbox.precaching.precacheAndRoute([
     { url: '/_nuxt/img/f1c895d-600.webp', revision: 'abcd1234' },
     { url: '/_nuxt/img/e670645-378.webp', revision: 'abcd1234' },
 ]);
-*/
+
 
 
 workbox.routing.registerRoute(
